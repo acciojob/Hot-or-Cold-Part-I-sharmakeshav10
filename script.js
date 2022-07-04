7a42a5fbf691386cfcfb9f9c3522fabdff03d207
Keshav Sharma <sharmakeshav0708@gmail.com>
2022-07-04 20:40:11 +0530

    random number generation for paragraph

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+const clickBtn = document.getElementById("btn");
+
+function generateRandomNum() {
+    clickBtn.addEventListener("click", function(){
+        const randomNum = Math.floor(Math.random()*41) - 20;
+        document.getElementById('num').textContent = randomNum;
+    })
+}
+
+generateRandomNum();
+
+
 
