0e33bc64f8bf61230009d76b11f4d75a21d1539c
Keshav Sharma <sharmakeshav0708@gmail.com>
2022-07-07 13:49:53 +0530

    hot and cold output changes

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 //your code here
 const clickBtn = document.getElementById("btn");
+const userInput = document.getElementById("guess");
 
 function generateRandomNum() {
     clickBtn.addEventListener("click", function(){
         const randomNum = Math.floor(Math.random()*41) - 20;
         document.getElementById('num').textContent = randomNum;
+
+        if(randomNum > (userInput.value - 5) && randomNum < (userInput.value + 5)) {
+            document.getElementById('respond').textContent = "Hot";
+        } else {
+            document.getElementById('respond').textContent = "Cold";
+        }
     })
 }
 
